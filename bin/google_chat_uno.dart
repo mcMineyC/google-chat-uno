31a5926d457524bfb8c468a2ccbd4855f16e05a5
mcMineyC <jedi.master@falconhosting.net>
2024-07-18 10:32:32 -0700

    maybe

--- bin/google_chat_uno.dart
+++ bin/google_chat_uno.dart
@@ -1,32 +1,15 @@
-import 'package:google_chat_uno/google_chat_uno.dart' as google_chat_uno;
-
-import 'package:shelf_router/shelf_router.dart';
-import 'package:shelf/shelf.dart';
-import 'package:shelf_cors_headers/shelf_cors_headers.dart';
-import 'package:shelf/shelf_io.dart' as io;
+import 'package:google_chat_uno/google_chat_uno.dart' as lib;
 
 import 'dart:io';
 import 'dart:convert';
 
 void main(List<String> arguments) async {
-  var app = Router();
-
-  app.get('/hello', (Request request) {
-    return Response.ok('hello-world');
-  });
-
-  app.get('/url/<id>', (Request request, String id) async {
-    return Response.ok(id.toString());
-  });
-  var handler = const Pipeline()
-    .addMiddleware(logRequests())
-    .addMiddleware(corsHeaders(originChecker: (origin) {
-      if(origin.contains('localhost')) return true;
-      else if(origin.contains('eatthecow.mooo.com')) return true;
-      else if(origin.contains('taxi-native.vercel.app')) return true;
-      else return false;
-    }))
-    .addHandler(app);
-  var server = await io.serve(handler, '0.0.0.0', 8080);
-  print("Server running at http://${server.address.host}:${server.port}");
+  while (true) {
+    String? input = stdin.readLineSync(encoding: utf8);
+    print("Hejo ${input}");
+    bool yn = stdin.readLineSync(encoding: utf8) == "y";
+    if (yn && input != null) {
+      lib.commit(input);
+    }
+  }
 }
--- lib/google_chat_uno.dart
+++ lib/google_chat_uno.dart
@@ -1,2 +1,35 @@
+import 'package:process_run/shell.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+import 'dart:io';
 Future commit(String message) async {
+  print("Committing: $message");
+  var path = 'garbage.txt';
+  var content = randomGarbage();
+  print("Garbage: $content");
+  var file = File(path).openWrite(mode: FileMode.write);
+  file.write(content);
+  print("File: $file");
+  print("Wrote junk");
+  var shell = Shell();
+  print('git add .\noutput:');
+  print(shell.runSync('git add .'));
+  print('git commit -m "$message"\noutput:');
+  print(shell.runSync('git commit -m "$message"'));
+  print('git push\noutput:');
+  print(shell.runSync('git push'));
+}
+
+Future writeGarbage() async {
+  var path = 'garbage.txt';
+  var content = randomGarbage();
+  var file = File(path);
+  await file.writeAsString(content);
+}
+
+String randomGarbage(){
+  var bytes = utf8.encode(DateTime.now().millisecondsSinceEpoch.toString()); // data being hashed
+  var digest = sha1.convert(bytes);
+  print(digest.toString());
+  return digest.toString();
 }
